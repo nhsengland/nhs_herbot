db92f34731bf1116701f7f813785f0101176f380
joseph.wilson8-nhs <joseph.wilson8@nhs.net>
2025-04-02 14:13:01 +0100

    Add module imports to initialise nhs_herbot package

---
+++ nhs_herbot/__init__.py
@@ -0,0 +1,8 @@
+"""
+Import all the necessary modules and classes for the NHS Herbot package.
+"""
+
+from nhs_herbot.errors import *  # noqa: F403, F401
+from nhs_herbot.joins import *  # noqa: F403, F401
+from nhs_herbot.load_csv import *  # noqa: F403, F401
+from nhs_herbot.utils import *  # noqa: F403, F401
